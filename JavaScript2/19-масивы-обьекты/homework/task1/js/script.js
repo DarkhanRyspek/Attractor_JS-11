5b7acf437dd66f101533a1529ebc3eb415f4c0bc
Darkhan Ryspek <darhanryspek@gmail.com>
2022-08-24 13:39:40 +0600

    JS-22

---
+++ JavaScript2/19-масивы-обьекты/homework/task1/js/script.js
@@ -0,0 +1,13 @@
+
+const yodaSays = ["on JavaScript", "programming ", "I like "];
+const message = yodaSays[2] + ' ' + yodaSays[1] + ' ' + yodaSays[0] + '.';
+
+console.log(message);
+/*
+Задание 1
+Дан массив:
+const yodaSays = ["on JavaScript", "programming ", "I like "];
+С помощью обращения к элементам массива составьте правильную строку и выведите ее на экран.
+Указания:
+Сдавать в виде HTML-файла.
+*/
---
+++ JavaScript2/19-масивы-обьекты/homework/task7/js/script.js
@@ -0,0 +1,44 @@
+/*
+//for
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+*/
+//do while
+let water = 0;
+do {
+    console.log('Add +1 liter water into the bucket');
+    water++;
+} while (water < 5);
+
+
+while (water < 4) {
+    console.log('Add +1 liter water into the bucket');
+    water++;
+}
+
+
+
+/*
+//масивы 
+const numbers = [4, 8, 15, 16, 23, 42];
+const drinks = ['beer', 'milk', 'whiskey', 'cola'];
+
+console.log(drinks);
+console.log(drinks[0]);
+//add data in array
+drinks.push('pina colada');
+console.log(drinks);
+
+//хешы или обьекты
+const myObject = {
+    name: 'Darkhan',
+    surname: 'Ryspek',
+    age: 30,
+    citizen: true
+}
+
+console.log(myObject);
+console.log(myObject.citizen);
+console.log(myObject['age']);
+*/
